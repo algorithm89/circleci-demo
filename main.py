4bff29d6907a25b163060783cb8cd6111df1477d
algorithm89 <alexei.go89@gmail.com>
2022-05-04 22:10:04 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def Add(a, b):
+    return a + b
+
+
+def SayHello():
+    print("sup world from srcmake")
+
+
+if __name__ == '__main__':
+    SayHello()
